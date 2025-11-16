07a611be57c04a59e3c8e0b49d3c7eabd5d18c79
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2025-11-16 17:54:07 +0100

    feat: add hexagon

---
+++ GratefulMoments/Custom Views/Hexagon.swift
@@ -0,0 +1,18 @@
+//
+//  Hexagon.swift
+//  GratefulMoments
+//
+//  Created by Adam Buryšek on 16.11.2025.
+//
+
+import SwiftUI
+
+struct Hexagon: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Hexagon()
+}
---
+++ GratefulMoments/Custom Views/HexagonLayout.swift
@@ -0,0 +1,8 @@
+//
+//  HexagonLayout.swift
+//  GratefulMoments
+//
+//  Created by Adam Buryšek on 16.11.2025.
+//
+
+import Foundation
