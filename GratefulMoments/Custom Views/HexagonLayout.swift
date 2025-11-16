68e0d9cefb6dfa5dccd9875d0e96a94f5ef056da
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2025-11-16 18:28:22 +0100

    feat: add hexagon

--- GratefulMoments/Custom Views/HexagonLayout.swift
+++ GratefulMoments/Custom Views/HexagonLayout.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+
+enum HexagonLayout {
+    case standard
+    case large
+
+
+    var size: CGFloat {
+        switch self {
+        case .standard:
+            return 200.0
+        case .large:
+            return 350.0
+        }
+    }
+}
