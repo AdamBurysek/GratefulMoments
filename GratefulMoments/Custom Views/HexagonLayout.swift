85e75cedfd5fdc8069ef84abb5c1fce66e05fca2
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2025-11-16 18:39:56 +0100

    feat: add moment hexagon view

--- GratefulMoments/Custom Views/HexagonLayout.swift
+++ GratefulMoments/Custom Views/HexagonLayout.swift
@@ -5,7 +5,7 @@
 //  Created by Adam Buryšek on 16.11.2025.
 //
 
-import Foundation
+import SwiftUI
 
 
 enum HexagonLayout {
@@ -19,6 +19,41 @@ enum HexagonLayout {
             return 200.0
         case .large:
             return 350.0
+        }
+    }
+
+
+    var timestampBottomPadding: CGFloat {
+        0.08
+    }
+
+
+    var textBottomPadding: CGFloat {
+        0.25
+    }
+
+
+    var timestampHeight: CGFloat {
+        size * (textBottomPadding - timestampBottomPadding)
+    }
+
+
+    var titleFont: Font {
+        switch self {
+        case .standard:
+            return .headline
+        case .large:
+            return .title.bold()
+        }
+    }
+
+
+    var bodyFont: Font {
+        switch self {
+        case .standard:
+            return .caption2
+        case .large:
+            return .body
         }
     }
 }
